82a26913447c709fe910d9e953531230bf45537c
NavarroWD <navarrowalterd@gmail.com>
2025-04-19 03:56:56 -0300

    subo ambos

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #define N 20 
 int main(){
+     srand(time(NULL));
 int i; 
 double vt[N]; 
 for(i = 0;i<N; i++){  
     vt[i]=1+rand()%100; 
-    printf("%f", vt[i]);
+    printf("%f \n", vt[i]);
 }
 }
 
