9fb6f0d8ef690dd77d77b279e31a4f33727d3fbc
NavarroWD <navarrowalterd@gmail.com>
2025-04-19 03:41:50 -0300

    agrego tp2_1_1.c

---
+++ tp2_1_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 20 
+int main(){
+int i; 
+double vt[N]; 
+for(i = 0;i<N; i++){  
+    vt[i]=1+rand()%100; 
+    printf("%f", vt[i]);
+}
+}
+
+/* 
+a) Complete el código anterior para que el mismo funcione en un archivo nuevo que se llame 
+tp2_1_1.c y agregue el archivo a su repositorio local (commit) y luego al repositorio 
+remoto (push).  
+Ejecute los siguientes comandos 
+● git add . 
+● git commit -m ‘inicial’ 
+● git push
+*/
